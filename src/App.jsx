22c0acffcfa425feec905bb2c32df6fdb916100f
kaarenzu <karenzcortes0211@gmail.com>
2025-12-29 01:19:59 -0300

    Add React router with main application routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import CrearReporte from "./pages/CrearReporte";
 import CambiarEstado from "./pages/CambiarEstado";
 
 function App() {
   return (
-    <div>
-      <Login />
-      <CrearReporte />
-      <CambiarEstado />
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/crear-reporte" element={<CrearReporte />} />
+      <Route path="/cambiar-estado" element={<CambiarEstado />} />
+    </Routes>
   );
 }
 
