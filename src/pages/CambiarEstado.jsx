2706fe6deb3f5298515dafc3b50bf4a6dbc88c0b
kaarenzu <karenzcortes0211@gmail.com>
2025-12-29 01:15:41 -0300

    Add frontend structure with pages for login and reports

---
+++ src/pages/CambiarEstado.jsx
@@ -0,0 +1,9 @@
+function CambiarEstado() {
+    return (
+        <div>
+            <h1>Cambiar Estado</h1>
+        </div>
+    );
+}
+
+export default CambiarEstado;
---
+++ src/pages/CrearReporte.jsx
@@ -0,0 +1,9 @@
+function CrearReporte() {
+    return (
+        <div>
+            <h1>Crear Reporte</h1>
+        </div>
+    );
+}
+
+export default CrearReporte;
